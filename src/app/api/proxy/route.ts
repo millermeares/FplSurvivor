a9d0bd782c3cee3b6454916bf2012c40378fcce1
Miller Meares <benmillermeares3@gmail.com>
2025-03-02 22:11:21 -0500

    fixing lint by using error

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -22,6 +22,7 @@ export async function POST(req: Request) {
     const data = await response.json();
     return NextResponse.json(data, { status: response.status });
   } catch (error) {
+    console.error(error)
     return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
